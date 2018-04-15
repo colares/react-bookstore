977a050fd69c889f57e8c02761b112896f6a7aba
Thiago Colares <thicolares@gmail.com>
2018-04-15 19:45:24 -0300

    Create the first reducer

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'The Sound and the Fury'},
+        { title: 'To Kill a Mockingbird'},
+        { title: 'Brave New World'},
+        { title: 'On the Road'},
+    ]
+}
